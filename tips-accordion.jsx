83d2fccc1891376a2b2da3fc9501fb27805e80dc
Bacon Chris <121662316+ChrisChang8@users.noreply.github.com>
2024-07-17 00:58:42 -0500

    Add files via upload

---
+++ tips-accordion.jsx
@@ -0,0 +1,29 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Diet & Health",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Muscle Growth",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Supplements",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Inspiration",
+    detail:
+      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+  }
+];
+export default data;
